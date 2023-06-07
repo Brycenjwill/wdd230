43e1904fa5ff5bf6bc96bf7f8b5faeb4aa0c45fa
Brycen Williams <brycenjwill@gmail.com>
2023-06-07 16:24:54 -0600

    Year now dynamically updated

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -1,3 +1,7 @@
 let modified = document.lastModified;
 
 document.getElementById("lastModified").innerHTML = `Last modification: ${modified}`;
+
+let curYear = new Date().getFullYear();
+
+document.getElementById("year").innerHTML = `&copy; ${curYear}`;
