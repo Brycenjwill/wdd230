971b9fe0f38eac467d36f8d1cf14c697a288586e
Brycen Williams <brycenjwill@gmail.com>
2023-06-15 18:49:06 -0600

    Added Night mode to header

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,7 +1,32 @@
 const ham = document.querySelector('#ham');
 const nav = document.querySelector('.nav');
+let hamb = 1;
 
 ham.addEventListener('click', () => {
 	nav.classList.toggle('open');
 	ham.classList.toggle('open');
+	if (hamb == 1){
+		nav.style.display = "block";
+	}
 });
+
+const night = document.querySelector("#night");
+const header = document.querySelector("header");
+
+
+const footer = document.querySelector("footer");
+let mode = 1;
+
+night.addEventListener("click", () => {
+	if (mode == 1) {
+		header.style.background = "#232323";
+		header.style.color = "#fff";
+
+		mode = 2;
+	} else {
+		header.style.background = "#4A4E69";
+		header.style.color = "#000";
+		mode = 1;
+	}
+});
+
