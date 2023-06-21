fa637784a08b72f5144c5593e10a8d54ca26f046
Brycen Williams <wil21062@byui.edu>
2023-06-21 14:03:06 -0600

    Fixed dark mode

--- scripts/menu.js
+++ scripts/menu.js
@@ -11,21 +11,28 @@ ham.addEventListener('click', () => {
 });
 
 const night = document.querySelector("#night");
-const header = document.querySelector("header");
-
+const main = document.querySelector("*");
+const h2 = document.querySelector("h2");
+const card1 = document.querySelector('.card1');
+const card2 = document.querySelector(".card2");
 
 const footer = document.querySelector("footer");
 let mode = 1;
 
 night.addEventListener("click", () => {
 	if (mode == 1) {
-		header.style.background = "#232323";
-		header.style.color = "#fff";
+		main.style.background = '#192B41';
+		h2.style.color = "white"
+		card1.style.background = 'lightgrey';
+		card2.style.background = "lightgrey";
 
 		mode = 2;
 	} else {
-		header.style.background = "#4A4E69";
-		header.style.color = "#000";
+		main.style.background = "white";
+		h2.style.color = "grey";
+		card1.style.background = "white";
+		card2.style.background = "white"
+		
 		mode = 1;
 	}
 });
