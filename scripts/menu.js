510c4e6be56d3bc78e3ebb17d433fffe954e45cf
Brycen Williams <brycenjwill@gmail.com>
2023-06-22 08:49:19 -0600

    Finished Wk3 team assignment

--- scripts/menu.js
+++ scripts/menu.js
@@ -14,7 +14,12 @@ const night = document.querySelector("#night");
 const main = document.querySelector("*");
 const h2 = document.querySelector("h2");
 const card1 = document.querySelector('.card1');
-const card2 = document.querySelector(".card2");
+const card2p = document.querySelectorAll(".card2 p ");
+const card2 = document.querySelector(".card2"); 
+const card1li = document.querySelectorAll(".card1 li"); 
+const card1a = document.querySelectorAll(".card1 a");
+const card1Hover = document.querySelectorAll(".card1 a:hover");
+
 
 const footer = document.querySelector("footer");
 let mode = 1;
@@ -23,17 +28,45 @@ night.addEventListener("click", () => {
 	if (mode == 1) {
 		main.style.background = '#192B41';
 		h2.style.color = "white"
-		card1.style.background = 'lightgrey';
-		card2.style.background = "lightgrey";
+		card1.style.background = 'black';
+		card2p.forEach(fontWhite);
+		card2.style.background = "black";
+		card1li.forEach(fontWhite);
+		card1a.forEach(fontWhite);
 
 		mode = 2;
 	} else {
 		main.style.background = "white";
 		h2.style.color = "grey";
 		card1.style.background = "white";
-		card2.style.background = "white"
-		
+		card2p.forEach(fontGrey);
+		card2.style.background = "white";
+		card1li.forEach(fontGrey);
+		card1a.forEach(fontBlack);
+
 		mode = 1;
 	}
 });
+function fontWhite(item){
+	item.style.color = "white";
+}
+function fontGrey(item){
+	item.style.color = "#5A5A5A"
+}
+function fontBlack(item){
+	item.style.color = "black";
+}
 
+
+
+const visits = document.querySelector(".visits");
+let numberOfVisits = Number(window.localStorage.getItem("numberOfVisits-ls")) || 0;
+if (numberOfVisits != 0){
+	visits.textContent = "Visits: "+numberOfVisits;
+}
+else{
+	visits.textContent = "This is the first visit! Welcome!"
+
+}
+numberOfVisits ++;
+localStorage.setItem("numberOfVisits-ls",numberOfVisits);
